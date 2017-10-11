5ed3461d8de5a60a4c35df2e8ac71bc4d5b579ac
dongbuihuu <silamy1927@gmail.com>
2017-10-11 22:04:24 +0700

    commit agin

--- Y2017JC79PRO/src/Teacher/Y2017JC79/arrays/dynamic_array/examples/EX02/Controllers.java
+++ Y2017JC79PRO/src/Teacher/Y2017JC79/arrays/dynamic_array/examples/EX02/Controllers.java
@@ -1,18 +1,19 @@
-package OOPS_CONCEPTS.ARRAY_AND_COLLECTION.DYNAMIC_ARRAYS.EXAMPLES.EX02;
+package Teacher.Y2017JC79.arrays.dynamic_array.examples.EX02;
 
 import java.util.List;
 
 public class Controllers {
 	private Models m_objModel;
+
 	public Controllers() {
 		m_objModel = new Models();
 	}
-	
-	public double TotalDouble(List<List<Double>> lisDouble){
+
+	public double TotalDouble(List<List<Double>> lisDouble) {
 		return m_objModel.TotalElements(lisDouble);
 	}
-	
-	public double TotalProduct(List<List<Product>> lisProduct){
+
+	public double TotalProduct(List<List<Product>> lisProduct) {
 		return m_objModel.TotalProductElements(lisProduct);
 	}
 }
--- Y2017JC79PRO/src/Teacher/Y2017JC79/arrays/dynamic_array/examples/EX02/Product.java
+++ Y2017JC79PRO/src/Teacher/Y2017JC79/arrays/dynamic_array/examples/EX02/Product.java
@@ -1,19 +1,22 @@
-package OOPS_CONCEPTS.ARRAY_AND_COLLECTION.DYNAMIC_ARRAYS.EXAMPLES.EX02;
+package Teacher.Y2017JC79.arrays.dynamic_array.examples.EX02;
 
 public class Product {
 	private int iProductId;
 	private int iQuantity;
-	
+
 	public int getiProductId() {
 		return iProductId;
 	}
+
 	public void setiProductId(int iProductId) {
 		this.iProductId = iProductId;
 	}
+
 	public int getiQuantity() {
 		return iQuantity;
 	}
+
 	public void setiQuantity(int iQuantity) {
 		this.iQuantity = iQuantity;
-	}	
+	}
 }
