41abfd9d2a7374c0cdf5ad5cb718b7e5c22050ae
nmtruong93 <nmtruong93@gmail.com>
2017-10-18 13:51:38 +0700

    First_Programming_Truongnm: ArrayList and Sum of ArrayList in double type

---
+++ Y2017JC79PRO/src/first_programming_truongnm/ArrayList_and_SumOfArrayList.java
@@ -0,0 +1,26 @@
+package first_programming_truongnm;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class MainApp {
+
+	public static void main(String[] args) {
+		ArrayList<Double> a = new ArrayList<Double>();
+		Scanner sc = new Scanner (System.in);
+		
+		for (int i = 1; i <= 5; i++) {
+			System.out.print("Kindly input a double number " + i + ": ");
+			a.add(sc.nextDouble());
+		}
+		sc.close();
+		double sum = 0;
+		for (Double double1 : a) {
+			sum = double1 + sum;
+		}
+		System.out.print("Sum of arraylist a is : ");
+		System.out.println(sum);
+	
+	}
+
+}
